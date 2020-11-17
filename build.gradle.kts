7ddb6fb01c9c784773559ad72701b9f959d53f4a
Romain Boisselle <romain@boisselle.dev>
2020-11-17 08:50:39 +0100

    kotlin 1.4.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    kotlin("js") version "1.3.72"
+    kotlin("js") version "1.4.10"
     `maven-publish`
 }
 
 group = "net.kodein.kpres"
-version = "1.0.0"
+version = "1.1.0"
 
 repositories {
     jcenter()
@@ -13,25 +13,24 @@ repositories {
 }
 
 kotlin {
-    target {
+    js {
         browser {}
         useCommonJs()
     }
 }
 
 dependencies {
-    implementation(kotlin("stdlib-js"))
-
-    val reactVersion = "16.13.1"
-    val reactRouterVersion = "5.1.2"
-    val kotlinWrapperVersion = "pre.110-kotlin-1.3.72"
+    val reactVersion = "17.0.0"
+    val reactRouterVersion = "5.2.0"
+    val styledVersion = "5.2.0"
+    val kotlinWrapperVersion = "pre.129-kotlin-1.4.10"
 
     api("org.jetbrains:kotlin-react-dom:$reactVersion-$kotlinWrapperVersion")
     api("org.jetbrains:kotlin-react-router-dom:$reactRouterVersion-$kotlinWrapperVersion")
-    api("org.jetbrains:kotlin-styled:1.0.0-$kotlinWrapperVersion")
+    api("org.jetbrains:kotlin-styled:$styledVersion-$kotlinWrapperVersion")
 
-    api(npm("highlight.js", "^10.1.2"))
-    api(npm("react-markdown", "^4.3.1"))
+    api(npm("highlight.js", "^10.3.2"))
+    api(npm("react-markdown", "^5.0.2"))
 }
 
 publishing {
